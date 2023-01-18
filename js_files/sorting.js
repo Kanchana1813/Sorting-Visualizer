30a058448983cc668346a3d8eff66ea226841294
Kanchana Ramu <gowda.kanchana1813@gmail.com>
2023-01-19 02:35:48 +0530

    update

---
+++ js_files/sorting.js
@@ -0,0 +1,14 @@
+function sorting() {
+    let array = new Array(100);
+
+    for(let i=0; i<100; i++){
+        array[i] = Math.floor(Math.random() * 100)+1;
+    }
+
+    for(let i=0; i<100; i++){
+        let barDiv = document.createElement("div");
+        barDiv.classList.add("barStyle");
+        barDiv.style.height = array[i] * 5 + "px";
+        document.getElementById("bars").appendChild(barDiv);
+    }    
+}
